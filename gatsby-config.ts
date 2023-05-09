97a17126291aadaea3497b2718aae27521fcc090
dawidurbanski <hi@dawidurbanski.com>
2023-05-09 09:42:40 +0200

    Clean init

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,28 +1,49 @@
 import type { GatsbyConfig } from "gatsby";
 
+require("dotenv").config({
+  path: `.env`,
+});
+
+if (!process.env.CONTENTFUL_ACCESS_TOKEN) {
+  throw new Error(
+    "CONTENTFUL_ACCESS_TOKEN not set. Please set it in your .env file."
+  );
+}
+
+if (!process.env.CONTENTFUL_SPACE_ID) {
+  throw new Error(
+    "CONTENTFUL_SPACE_ID not set. Please set it in your .env file."
+  );
+}
+
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `GrowthBook Testing`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "BvwF_qfTUu-edLvVuepsG21Ewafm2Km3QbR5MYDMScE",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-emotion", "gatsby-plugin-google-gtag", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        environment: process.env.CONTENTFUL_ENVIRONMENT || "master",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-emotion",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
 
 export default config;
